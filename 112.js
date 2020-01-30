d3ede371d9a048afe3f70a5cf90e971a2ac08991
Ysa <deysaherdy@hotmail..com>
2020-01-29 22:45:47 -0300

    --- Auto-Git Commit ---

--- 112.js
+++ 112.js
@@ -1,3 +1,7 @@
 const produtos = [
     {nome: 'Notebook', preco: 2499, fragil: true},
+    {nome: 'Ipad Pro', preco: 4919, fragil: true},
+    {nome: 'Copo de Vidro', preco: 12.49, fragil: true},
+    {nome: 'Copo de Plástico', preco: 18.99, fragil: false}
+
 ]
