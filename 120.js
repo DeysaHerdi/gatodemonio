620fbc2a1418c2d9f1fb040bfe0603f3a4dd4032
Ysa <deysaherdy@hotmail..com>
2020-02-09 01:34:35 -0300

    --- Auto-Git Commit ---

--- 120.js
+++ 120.js
@@ -26,3 +26,9 @@ const escola = [
 
 const eAgora = escola.reduce( (acumulador, turma) => acumulador.concat(turma.alunos), []).map(aluno => aluno.nota)
 console.log(eAgora)
+
+
+//Podemos primeiro juntar todos os alunos em um único array
+//const resultado1 = escola.reduce( (acumulador, turma) => acumulador.concat(turma.alunos) , [])
+//primeiro: acumulador = [], turma = {nome, alunos[alunos1 e aluno2]} [].concat(aluno1, aluno2)
+//segunda acumulador = [aluno1, aluno2], turma = {nome, alunos[aluno3, aluno4]} 
