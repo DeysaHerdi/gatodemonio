36cc34581f2a53a136b2707facec7d9f63106423
Ysa <deysaherdy@hotmail..com>
2020-02-10 03:28:47 -0300

    --- Auto-Git Commit ---

--- Usuario.js
+++ Usuario.js
@@ -1,7 +1,12 @@
 let usuario = 'admin'
 let senha = 'admin'
+let numeroDeTentaivas = 0
 
 this.logar = (login, passaword) =>{
+    if(numeroDeTentaivas == 3){
+        console.log('Você não pode logar mais no sistema')
+        return;
+    }
     if(login == usuario && passaword == senha){
         console.log('Parabens voce conseguiu logar no sietama')
         }else{
