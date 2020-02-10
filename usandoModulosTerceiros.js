eca9cd30518000933b326594f7a1ad5be87f2687
Ysa <deysaherdy@hotmail..com>
2020-02-10 04:14:34 -0300

    --- Auto-Git Commit ---

--- usandoModulosTerceiros.js
+++ usandoModulosTerceiros.js
@@ -2,4 +2,6 @@ const _ = require('lodash')
 //setInterval(() => console.log(_.random(10, 60)), 2000)
 
 const cep = require('cep-promise')
-cep(59064900).then(valor => console.log(valor))
+//cep(59064900).then(valor => console.log(valor))
+
+const postalCodes = require('postal-codes-js');
