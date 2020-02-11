888fff58c36d14c71c570f6a91def681cd58135e
Ysa <deysaherdy@hotmail..com>
2020-02-10 21:15:27 -0300

    --- Auto-Git Commit ---

--- funcionarios/funcionarios.js
+++ funcionarios/funcionarios.js
@@ -4,9 +4,10 @@ const axios = require('axios')
 axios.get(url).then(response => {
     const funcionarios = response.data
 
-    const funcionariosChineses = funcionarios.filter(funcionario => funcionario.pais == 'China')
-    
     const funcionariasMulheres = funcionarios.filter(funcionario => funcionario.genero == 'F')
 
-    console.log(`São chineses o total de ${funcionariosChineses.length}`)
+    const funcionariosChineses = funcionarios.filter(funcionario => funcionario.pais == 'China')
+    
+    
+    console.log(`São chineses o total de ${funcionariosChineses.length} e ${funcionariasMulheres.length} são mulheres!`)
 })
