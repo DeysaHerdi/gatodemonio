cee409b3d8486bed9761f11a31873f67c4475de9
Ysa <deysaherdy@hotmail..com>
2020-02-11 17:33:18 -0300

    --- Auto-Git Commit ---

--- funcionarios/funcionarios.js
+++ funcionarios/funcionarios.js
@@ -1,12 +1,15 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
-axios.get(url).then(response => {
-    const funcionarios = response.data
-    const fMulheres = funcionarios.filter(funcionario => funcionario.genero == 'F')
-    const fChineses = funcionarios.filter(funcionario => funcionario.pais == 'China')
-    const salarios = funcionarios.map(funcionario => funcionario.salario)
+axios.get(url).then(res => {
+    const funcionarios = res.data
+
+    const xxxxx = funcionarios.filter(funcionario => funcionario.pais === 'China')
+                              .filter(funcionario => funcionario.salario > 22000)
+                              .filter(funcionario => funcionario.genero === 'F')
+                              .map(funcionario => funcionario.nome)
     
-    
-    console.log(`São chineses o total de ${fChineses.length} e ${fMulheres.length} são mulheres!`)
+    const ultimoXXXXX = xxxxx[xxxxx.length-1];
+
+    console.log(ultimoXXXXX.substr(0,3)) 
 })
