7ecb76557d9b12646e6b768d1da03ef6156bd1b1
Ysa <deysaherdy@hotmail..com>
2020-02-14 02:03:35 -0300

    --- Auto-Git Commit ---

--- Jogu.js
+++ Jogu.js
@@ -3,6 +3,6 @@ const BatalhaPokemon = require('./BatalhaPokemon')
 
 let charizard = new Pokemon('charizard', 'fogo')
 let squirtle = new Pokemon('squirtle', 'agua')
-let Devar = new Blacklist('Devar', 'Trevas','')
+let Devar = new Blacklist('Devar', 'Trevas')
 
 BatalhaPokemon.luta(charizard, squirtle)
