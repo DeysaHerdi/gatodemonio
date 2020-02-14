a101760a68c6b323700ed70935f997284926947b
Ysa <deysaherdy@hotmail..com>
2020-02-13 21:32:06 -0300

    --- Auto-Git Commit ---

--- jogo.js
+++ jogo.js
@@ -2,3 +2,6 @@ const Loja = require('./Loja')
 const Campeao = require('./Campeao')
 
 Campeao.quantosCampeoes();
+Campeao.removerDoLol('Braum')
+Campeao.removerDoLol('Taric')
+Campeao.quantosCampeoes();
