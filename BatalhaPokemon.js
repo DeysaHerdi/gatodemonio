8d5aaaa0bd163cba7d5a77343cbcb8efa5f87c09
Ysa <deysaherdy@hotmail..com>
2020-02-14 02:06:40 -0300

    --- Auto-Git Commit ---

--- BatalhaPokemon.js
+++ BatalhaPokemon.js
@@ -2,7 +2,12 @@ const luta = (pokemon1, pokemon2) => {
     pokemon1.atacar(pokemon2)
     pokemon2.atacar(pokemon1)
 }
+const luta2 = (Blaclist1,Blacklist2) => {
+    Blaclist1.atacar(Blacklist2)
+    Blacklist2.atacar(Blaclist1)
+}
 //aqui eu preferi voltar um objeto
 module.exports = {
     luta
+    luta2
 }
--- Jogu.js
+++ Jogu.js
@@ -3,6 +3,6 @@ const BatalhaPokemon = require('./BatalhaPokemon')
 
 let charizard = new Pokemon('charizard', 'fogo')
 let squirtle = new Pokemon('squirtle', 'agua')
-let Devar = new Blacklist('Devar', 'Trevas')
+let Devar = new Blacklist('Devar', 'Trevas','Para de mastigar Aaron')
 
 BatalhaPokemon.luta(charizard, squirtle)
