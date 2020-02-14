2d04feedb68dc8b28ccaf64f314b44bc22b22ff6
Ysa <deysaherdy@hotmail..com>
2020-02-13 21:53:42 -0300

    --- Auto-Git Commit ---

--- Campeao.js
+++ Campeao.js
@@ -4,3 +4,10 @@ const removerDoLol = campeaoParaRemover => {
     campeoes = campeoes.filter(campeao => campeao.name != campeaoParaRemover)
     console.log(`----- removi o ${campeaoParaRemover} do lol graças a deus -----`)
 }
+
+const quantosCampeoes = () => console.log(`Lol possi ${campeoes.length}`)
+
+module.exports = {
+    removerDoLol,
+    quantosCampeoes
+}
