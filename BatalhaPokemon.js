668aab30f0f6a082bfdf81e369246f28adfb2ac0
Ysa <deysaherdy@hotmail..com>
2020-02-14 02:09:46 -0300

    --- Auto-Git Commit ---

--- BatalhaPokemon.js
+++ BatalhaPokemon.js
@@ -2,12 +2,12 @@ const luta = (pokemon1, pokemon2) => {
     pokemon1.atacar(pokemon2)
     pokemon2.atacar(pokemon1)
 }
-const luta2 = (Blaclist1,Blacklist2) => {
-    Blaclist1.atacar(Blacklist2)
-    Blacklist2.atacar(Blaclist1)
+const luta2 = (blaclist1,blacklist2) => {
+    blaclist1.atacar2(blacklist2)
+    blacklist2.atacar2(blaclist1)
 }
 //aqui eu preferi voltar um objeto
 module.exports = {
-    luta
+    luta,
     luta2
 }
