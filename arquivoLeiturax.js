1de5b1e31fba866b8fdf0f818f1fb41c87c5076e
Ysa <deysaherdy@hotmail..com>
2020-02-15 05:41:49 -0300

    --- Auto-Git Commit ---

--- arquivoLeiturax.js
+++ arquivoLeiturax.js
@@ -1,9 +1,12 @@
 const fs = require('fs')
 
-const caminhoDoArquivo = `${__dirname}/aquivo.x.txt`)
+const caminhoDoArquivo = `${__dirname}/aquivo.x.txt`
 fs.readFile(caminhoDoArquivo, 'utf-8', (erro,conteudo)=>{
     if(erro){
-        console.log('')
-}
-
-}
+        console.log(`erro ao ler arquivo, erro:
+        ${erro}`)
+} else{
+    console.log('Conteudo do arquivo é:')
+    console.log(conteudo)
+    }
+})
