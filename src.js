c6a70fbb9d863da16dce0dc093c856837ee22e96
Ysa <deysaherdy@hotmail..com>
2020-02-19 21:03:02 -0300

    --- Auto-Git Commit ---

--- src.js
+++ src.js
@@ -2,6 +2,16 @@ const app = require('express')()
 
 let contador = 0;
 
-const getContador = (req, res) =>{
+const getContador = (req, res) => {
     res.send(`O contador vale ${contador}`)
+}
+
+const atualizarContador = (req, res) => {
+    contador++
+    res.send(`O contador foi atualizado!`)
+}
+
+const resetaContador = (req, res) => {
+    contador = 0
+
 }
