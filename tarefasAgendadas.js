cc698345348099ce0fd3a7b4a6637b3a108dae12
Ysa <deysaherdy@hotmail..com>
2020-02-22 16:09:20 -0300

    --- Auto-Git Commit ---

--- tarefasAgendadas.js
+++ tarefasAgendadas.js
@@ -1,2 +1,9 @@
-const schedule = require('nodes-schedule')
+const schedule = require('node-schedule')
 const regra = new schedule.RecurrenceRule()
+
+regra.diaDaSemana = [0]
+regra.hora = 0
+regra.minuto = 1
+schedule.scheduleJob(regra, function(){
+    console.log("Hora de comer Devar!!!")
+})
